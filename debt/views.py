85ce252d32ac387b135fd8d3e60c746bce6b9962
Rosalina Castano <rthomas1210@gmail.com>
2017-02-21 21:12:02 -0600

    getting started on mortgage view

--- debt/views.py
+++ debt/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import View
 
-# Create your views here.
+
+class MortgageCalculatorView(View):
+    def get(self, request):
+        template_vars = {
+            'balance': 150000,
+            'interest_rate': 4.5,
+            'loan_terms': [10, 15, 30],
+        }
+        return render(request, 'mortgage-calculator.html', template_vars)
+
